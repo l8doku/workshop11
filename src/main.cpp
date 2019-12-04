54e36d9c6a839affc9be83e0dcf4db4927455ec9
Georgii Zhulikov <l8doku@gmail.com>
2019-12-04 11:35:10 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <utility> 
+#include <iostream>
+
+
+double calcSum(const std::vector<std::pair<double, double> >& nums)
+{
+    // Write your code here
+}
+
+/* */
+int main ()
+{
+    std::vector<std::pair<double, double> > test1 = {{10, 20}, {20, 30}};
+    double result = calcSum(test1);
+    std::cout << result;
+
+}
+/* */
